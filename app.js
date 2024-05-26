b70d8900c6503f0593a161a8802fcb0443fe8587
mathesfonseca18 <matheus.fonseca0218@gmail.com>
2024-05-26 00:59:17 -0300

    comparando chute

--- app.js
+++ app.js
@@ -2,45 +2,53 @@ let numero1;
 let numero2;
 let numeroMaior;
 let numeroMenor;
+let numeroSorteado;
 
-function exibirTextoNaTela (tag, texto) {
-    let campo  = document.querySelector(tag);
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 function sortear() {
+    event.preventDefault();
 
-event.preventDefault();
+    numero1 = parseInt(document.querySelector('.inputNumero1').value);
+    numero2 = parseInt(document.querySelector('.inputNumero2').value);
 
-numero1 = parseInt(document.querySelector('.inputNumero1').value);
-numero2 = parseInt(document.querySelector('.inputNumero2').value);
+    document.querySelector('.sortearNumero').setAttribute('disabled', 'disabled');
 
-document.querySelector('.sortearNumero').setAttribute('disabled', 'disabled')
+    if (numero1 > numero2) {
+        numeroMaior = numero1;
+        numeroMenor = numero2;
+    } else {
+        numeroMaior = numero2;
+        numeroMenor = numero1;
+    }
 
-if (numero1 > numero2) {
-    numeroMaior = numero1
-    numeroMenor = numero2;
-} else {
-    numeroMaior = numero2
-    numeroMenor = numero1;
-};
+    exibirTextoNaTela('.intervaloSorteado', `O número sorteado está entre ${numeroMenor} e ${numeroMaior}`);
 
-exibirTextoNaTela ('.intervaloSorteado', `O número sorteado está entre ${numeroMenor} e ${numeroMaior}`)
+    console.log(`numero1 = ${numero1}, numero2 = ${numero2}`);
+    console.log(`número menor = ${numeroMenor}, número maior = ${numeroMaior}`);
 
-console.log(`numero1 = ${numero1}, numero2 = ${numero2}`);
-console.log(`numeroMaior = ${numeroMaior}, numeroMenor = ${numeroMenor}`);
+    numeroSorteado = parseInt(Math.random() * (numeroMaior - numeroMenor + 1)) + numeroMenor;
 
-let sorteador = parseInt(Math.random() * numeroMaior + 1);
+    exibirTextoNaTela('#numeroSorteado', 'Já temos o número sorteado, faça seu chute!');
 
-while (sorteador < numeroMenor) {
-    console.log(`Número sorteado ${sorteador}, menor que ${numeroMenor}. Sortear novamente`)
-    sorteador = parseInt(Math.random() * numeroMaior + 1 );
- 
+    console.log (numeroSorteado)
 }
 
-exibirTextoNaTela('#numeroSorteado', `já temos o número sorteado, faça seu chute!`)
+function testarChute() {
+    event.preventDefault();
 
-console.log(sorteador);
+    let chute = parseInt(document.querySelector('.chute').value);
 
+    if (chute === numeroSorteado) {
+        exibirTextoNaTela('#numeroSorteado', 'Você acertou!');
+    } else {
+        if (chute > numeroSorteado) {
+            exibirTextoNaTela('#numeroSorteado', `O número sorteado é menor que ${chute}`);
+        } else {
+            exibirTextoNaTela('#numeroSorteado', `O número sorteado é maior que ${chute}`);
+        }
+    }
 }
-
